2f9ed7ce40c4af34505a32ddfef03bef78746c10
wol4aravio <wol4aravio@yandex.ru>
2018-07-05 15:02:15 +0300

    AdaptiveSearchNode

--- Algorithms/CSharp/HybridRandomSearch/HybridRandomSearch.cs
+++ Algorithms/CSharp/HybridRandomSearch/HybridRandomSearch.cs
@@ -30,6 +30,7 @@ namespace OSOL.Extremum.Algorithms.CSharp
         private static string minDeltaForSolutionVectorName = "minDeltaForSolutionVector"; // eps_2
 
         private static string currentSolutionName = "currentSolution"; // x
+        private static string currentSolutionEfficiencyName = "currentSolutionEfficiency"; // f_x
 
         private static GoRN gorn = new GoRN();
 
@@ -43,16 +44,91 @@ namespace OSOL.Extremum.Algorithms.CSharp
 
             public override void Initialize(Func<Dictionary<string, double>, double> f, Area area, State<RealVector, double, RealVector> state)
             {
-
                 RealVector x = area.ToDictionary(kvp => kvp.Key, kvp => 0.5 * (kvp.Value.Item1 + kvp.Value.Item2));
                 state.SetParameter(currentSolutionName, x);
+                state.SetParameter(currentSolutionEfficiencyName, x.GetPerformance(f));
             }
 
-            public override void Process(Func<Dictionary<string, double>, double> f, Area area,
-                State<RealVector, double, RealVector> state)
+            public override void Process(Func<Dictionary<string, double>, double> f, Area area, State<RealVector, double, RealVector> state)
             {
 
             }
+            
         }
+          
+        private class AdaptiveSearchNode : GeneralNode<RealVector, double, RealVector>
+        {
+
+            public AdaptiveSearchNode(int nodeId)
+            {
+                this.NodeId = nodeId;
+            }
+
+            public override void Initialize(Func<Dictionary<string, double>, double> f, Area area, State<RealVector, double, RealVector> state)
+            {
+                
+            }
+
+            public override void Process(Func<Dictionary<string, double>, double> f, Area area, State<RealVector, double, RealVector> state)
+            {
+                var x = state.GetParameter<RealVector>(currentSolutionName);
+                var f_x = state.GetParameter<double>(currentSolutionEfficiencyName);
+
+                var alpha = state.GetParameter<double>(expansionCoefficientName);
+                var beta = state.GetParameter<double>(compressionCoefficientName);
+                
+                var M = state.GetParameter<int>(numberOfBadTrialsName);
+                var N1 = state.GetParameter<int>(numberOfIterationsOfAdaptiveSearchName);
+                
+                var t_min = state.GetParameter<double>(minStepValueName);
+                
+                var t = 1.0;
+                var k = 0;
+                
+                for (int j = 0; j < N1; ++j)
+                {
+                    var xi = gorn.GetContinuousUniformVector(area.ToDictionary(
+                        kvp => kvp.Key, 
+                        kvp => Tuple.Create(-1.0, 1.0)));
+                    var xi_norm = Math.Sqrt(xi.Select(kvp => kvp.Value * kvp.Value).Sum());
+
+                    var x_new = x.MoveBy(xi.ToDictionary(kvp => kvp.Key, kvp => t * kvp.Value / xi_norm));
+                    x_new = x_new.Constrain(area);
+                    
+                    var f_x_new = x_new.GetPerformance(f);
+                    if (f_x_new < f_x)
+                    {
+                        var z = x.MoveBy(((x_new - x) * alpha).Elements);
+                        var f_z = z.GetPerformance(f);
+
+                        x = x_new;
+
+                        if (f_z < f_x_new)
+                        {
+                            x = z;
+                            f_x = f_z;
+                            t *= alpha;
+                            continue;
+                        }
+                    }
+
+                    k++;
+                    if (k == M)
+                    {
+                        k = 0;
+                        if (t < t_min)
+                        {
+                            return;
+                        }
+                        else
+                        {
+                            t *= beta;
+                        }
+                    }
+                }
+            }
+            
+        }
+
     }
 }
